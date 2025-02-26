2d6cdbf6004c7e298bf97b662e1c08b628a7d5ce
Yujun <pathfinder357@github.com>
2025-02-26 19:43:30 +0900

    lv 1-1: Early Return

--- src/main/java/org/example/expert/domain/auth/service/AuthService.java
+++ src/main/java/org/example/expert/domain/auth/service/AuthService.java
@@ -25,27 +25,33 @@ public class AuthService {
 
     @Transactional
     public SignupResponse signup(SignupRequest signupRequest) {
-
-        String encodedPassword = passwordEncoder.encode(signupRequest.getPassword());
-
-        UserRole userRole = UserRole.of(signupRequest.getUserRole());
-
+        // 1. 이메일 중복 검사
         if (userRepository.existsByEmail(signupRequest.getEmail())) {
             throw new InvalidRequestException("이미 존재하는 이메일입니다.");
         }
-
+        // 2. 비밀번호 암호화(중복이 없을때만 수행)
+        String encodedPassword = passwordEncoder.encode(signupRequest.getPassword());
+        // 3. UserRole 변환
+        UserRole userRole = UserRole.of(signupRequest.getUserRole());
+        // 4. 새로운 유저 생성 및 저장
         User newUser = new User(
                 signupRequest.getEmail(),
                 encodedPassword,
                 userRole
         );
         User savedUser = userRepository.save(newUser);
-
+        // 5. JWT 토큰 생성
         String bearerToken = jwtUtil.createToken(savedUser.getId(), savedUser.getEmail(), userRole);
 
         return new SignupResponse(bearerToken);
     }
-
+    /* 리펙토링된 코드의 동작 흐름
+    1. 이메일 중복 검사: userRepository.existsByEmail()을 통해 이메일 중복여부 먼저 확인
+        중복된 이메일 있으면 InvalidRequestException을 던짐.
+    2. 비밀 번호 암호화
+    3. 유저 생성
+    4. JWT
+    */
     @Transactional(readOnly = true)
     public SigninResponse signin(SigninRequest signinRequest) {
         User user = userRepository.findByEmail(signinRequest.getEmail()).orElseThrow(
